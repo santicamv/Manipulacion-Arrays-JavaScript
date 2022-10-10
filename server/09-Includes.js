dacd5962b0270828cfc52ab8450efc0ec0bc20c2
Santiago Camacho <santiagocamachovalbuena96@gmail.com>
2022-10-10 18:06:35 -0500

    Quinto commit que incluye al método includes

---
+++ server/09-Includes.js
@@ -0,0 +1,10 @@
+/*El método includes determina si un array o string incluye un determinado elemento. Devuelve true o false, si existe o no respectivamente.*/
+
+/*Arreglo que se construye para el ejemplo*/
+const pets = ["cat","dog","bat"];
+
+/*Utilizamos includes con el fin de buscar el elemento cat, en este caso como si se encuentra retorna true*/
+const ejemploIncludes = pets.includes("cat");
+
+/*Imprimimos por consola el resultado del ejemplo anterior*/
+console.log(ejemploIncludes);
